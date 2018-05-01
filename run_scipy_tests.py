10307d84feaf66cd8a373d1877cf7f63e7ed6db3
Matthew Brett <matthew.brett@gmail.com>
2018-05-01 18:34:21 +0100

    Try again with test selection

--- run_scipy_tests.py
+++ run_scipy_tests.py
@@ -1,5 +1,5 @@
 """
-run_scipy_tests.py TEST_MODE [-- PYTEST_ARGS..]
+run_scipy_tests.py [-- PYTEST_ARGS..]
 
 Run scipy tests allowing for pytest and nosetests
 """
@@ -10,9 +10,9 @@ import argparse
 
 def main():
     p = argparse.ArgumentParser(usage=__doc__.strip())
+    p.add_argument('--mode', default='fast')
     p.add_argument('--tests', action='append',
                    help="Specify tests to run")
-    p.add_argument('test_mode', metavar='TEST_MODE')
     p.add_argument('pytest_args', metavar='PYTEST_ARGS', nargs='*')
     args = p.parse_args()
     if len(args.tests) == 0:
@@ -20,7 +20,7 @@ def main():
 
     import scipy
     print("Scipy: {} {}".format(scipy.__version__, scipy.__path__))
-    ret = scipy.test(args.test_mode, extra_argv=args.pytest_args,
+    ret = scipy.test(args.mode, extra_argv=args.pytest_args,
                      tests=args.tests)
 
     if hasattr(ret, 'wasSuccessful'):
